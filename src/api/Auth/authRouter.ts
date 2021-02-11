73cee8ff240944664949f4f5091076a03a289ea5
taiwofayipe <taiwofayipe@gmail.com>
2021-02-11 16:27:14 +0100

    updated the request rule

--- src/api/Auth/authRouter.ts
+++ src/api/Auth/authRouter.ts
@@ -1,13 +1,15 @@
+import { validation } from './../../middleware/validation';
 import express from "express";
 import { controllerHandler } from "./../../shared/controllerHandler";
 import { AuthController } from "./authController";
+import { payloadValidation } from './authValidation';
 
 const router = express.Router();
 const call = controllerHandler;
 const Auth = new AuthController();
 
 
-router.get("/", call(Auth.getUserDetails, (req, res, next) => []));
+router.get("/", validation(payloadValidation), call(Auth.getUserDetails, (req, res, next) => []));
 
 router.post("/validate-rule",
   call(Auth.validateRules, (req, res, next) => [req.body]));
